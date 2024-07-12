a636bb5a1a3c9522dc842727cd8b4560f846f267
Sarath61 <sarathsai4602@gmail.com>
2024-07-12 22:20:10 +0530

    Updated Blog component

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { Article } from "../../components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./import";
 import "./blog.css";
+
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="gpt4__blog section_padding" id="blog">
+      <div className="gpt4__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt4__blog-container">
+        <div className="gpt4__blog-container_groupA">
+          <Article
+            imgUrl={blog01}
+            date="July 12, 2024"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+        <div className="gpt4__blog-container_groupB">
+          <Article
+            imgUrl={blog02}
+            date="July 12, 2024"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog03}
+            date="July 12, 2024"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog04}
+            date="July 12, 2024"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog05}
+            date="July 12, 2024"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
